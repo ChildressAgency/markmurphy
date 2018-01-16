9a609245920b2ad3ead80b7def70da0e07ffa64a
childressdev <jcampbell@childressagency.com>
2018-01-16 13:37:07 -0500

    minor tweaks

--- page-practice.php
+++ page-practice.php
@@ -15,7 +15,8 @@
             <?php
               $charges = new WP_Query(array(
                 'post_type' => 'charges',
-                'posts_per_page' => -1
+                'posts_per_page' => -1,
+                'post_status' => 'publish'
               ));
 
               if($charges->have_posts()): $i=0; while($charges->have_posts()): $charges->the_post(); 
@@ -23,10 +24,10 @@
                 <div class="col-sm-4">
                   <div class="charge-type">
                     <h3><?php the_title(); ?></h3>
-                    <?php if(have_rows('details_section')): ?>
+                    <?php if(have_rows('details_sections')): ?>
                       <ul class="list-unstyled">
-                        <?php $c=1; while(have_rows('details_section') && $c < 3): the_row(); ?>
-                          <li><?php echo get_field('detail_section_title') ? get_field('detail_section_title') : '&nbsp;'; ?></li>
+                        <?php $c=0; while(have_rows('details_sections') && $c < 3): the_row(); ?>
+                          <li><?php echo get_sub_field('detail_section_title') ? get_sub_field('detail_section_title') : '&nbsp;'; ?></li>
                         <?php $c++; endwhile; ?>
                       </ul>
                     <?php endif; ?>
@@ -43,13 +44,13 @@
 <section id="practiceAreas">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-9">
+      <div class="col-sm-9 col-sm-offset-3">
         <div class="practice-areas-descriptions">
           <h1>Areas of practice</h1>
           <div class="row">
             <div class="col-sm-6">
               <?php 
-                $total_charges = count($charges);
+                $total_charges = wp_count_posts('charges')->publish;
                 $half_total_charges = $total_charges/2;
                 $p=1;
                 while($charges->have_posts()): $charges->the_post(); ?>
